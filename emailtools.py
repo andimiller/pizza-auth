508cb099edf91dd2f511a9cc5965630a9101d601
Andi Miller <andi@andimiller.net>
2013-09-21 17:27:01 +0100

    Fixed the email module to work with global config values

--- emailtools.py
+++ emailtools.py
@@ -8,13 +8,14 @@ import json
 class EmailTools():
 
 	def __init__(self, config):
+		self.config = config
 		self.env = Environment()
 		self.env.loader = FileSystemLoader('templates/email')
 
 	def send_email(self, to, subject, body):
 		msg = MIMEText(body)
 		msg['Subject'] = subject
-		msg['From'] = "auth@" + config["auth"]["domain"]
+		msg['From'] = "auth@" + self.config["auth"]["domain"]
 		msg['To'] = to
 		msg['Precedence'] = "bulk"
 		msg['Auto-Submitted'] = 'auto-generated'
