dffbb132a8b2332e0f60fa281acaabd329123baf
Andi Miller <andi@andimiller.net>
2013-10-14 17:39:12 +0100

    Added url/autor to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup (
 	name='pizza_auth',
 	version='0.1',
 	packages=['pizza_auth'],
+	url='https://bitbucket.org/Sylnai/pizza-auth',
+	author="Andi Miller",
+	author_email="andi at andi miller dot net"
 	license='MIT License',
 	long_description="LDAP-Backed Web Application for the management of EVE Online players and services.",
 	classifiers=[
