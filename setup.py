ec5c40c11dc715bc0f3ba5231e7adb398bff6992
Andi Miller <andi@andimiller.net>
2013-10-14 17:24:51 +0100

    Refactoring into modular setup suitable for pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup (
+	name='pizza_auth',
+	version='0.1',
+	packages=['pizza_auth'],
+	license='MIT License',
+	long_description="LDAP-Backed Web Application for the management of EVE Online players and services.",
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Topic :: Internet',
+		'Topic :: Communications',
+		'Intended Audience :: Developers',
+		'Development Status :: 3 - Alpha',
+	]
+)
+
